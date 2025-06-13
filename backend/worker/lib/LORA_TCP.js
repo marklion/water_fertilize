a4461c3ea4ad2d35ee06ca596d10927413833353
marklion <marklion@sina.com>
2025-06-13 14:14:17 +0800

    adjust some structure

---
+++ backend/worker/lib/LORA_TCP.js
@@ -0,0 +1,8 @@
+module.exports = {
+    dev2tcp:async function(device) {
+        return {
+            ip:'1.2.3.4',
+            port: 502,
+        }
+    },
+};
---
+++ backend/worker/lib/modbus_dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+    dev2cmd:async function(device) {
+        return '0215563D'
+    },
+    reply2dev:async function(device, reply) {
+    },
+    error2dev:async function(device, err_msg) {
+    }
+};
